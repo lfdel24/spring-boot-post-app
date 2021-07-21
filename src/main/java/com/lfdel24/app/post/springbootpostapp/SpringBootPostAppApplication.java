c9106befce33dbf0a152d654989886c543722342
lfdel24 <lfdel24@gmail.com>
2021-07-20 21:05:46 -0500

    first commit

---
+++ src/main/java/com/lfdel24/app/post/springbootpostapp/SpringBootPostAppApplication.java
@@ -0,0 +1,13 @@
+package com.lfdel24.app.post.springbootpostapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lfdel24/app/post/springbootpostapp/SpringBootPostAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfdel24.app.post.springbootpostapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
