e2a252aa649567020d401e141d8bdc31e990cde7
lfdel24 <lfdel24@gmail.com>
2021-07-23 16:22:36 -0500

    add validator and lombok

--- src/main/java/com/lfdel24/app/post/postapp/entity/Post.java
+++ src/main/java/com/lfdel24/app/post/postapp/entity/Post.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Data;
 
 /**
  *
  * @author leo
  */
 @Entity(name = "posts")
+@Data
 public class Post implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -26,54 +28,4 @@ public class Post implements Serializable {
         this.title = "";
         this.body = "";
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Post)) {
-            return false;
-        }
-        Post other = (Post) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "com.lfdel24.app.post.postapp.entity.Post[ id=" + id + " ]";
-    }
-
 }
